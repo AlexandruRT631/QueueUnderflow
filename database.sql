3cfb7cf08173a9ca0463d9de00e0ae5dba29ae5b
AlexandruRT631 <allextodor@yahoo.com>
2023-05-29 13:18:57 +0300

    updated documentation

--- database.sql
+++ database.sql
@@ -82,6 +82,6 @@ insert into answers (user_id, content, question_id) values
 (2, '2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 1);
 
 insert into answer_votes values
-(2, 2, true), (2, 3, true);
+(2, 1, true), (2, 3, true);
 
 select * from answers
